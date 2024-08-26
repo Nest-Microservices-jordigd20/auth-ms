819f975af396dcd78c1d65f8229bb314104c0a95
jordigd20 <jordigd8@gmail.com>
2024-08-26 14:27:23 +0200

    feat: add environment config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('AuthMS - Main')
+  
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers
+    }
+  });
+
+  await app.listen();
+
+  logger.log(`AuthMS is running on port: ${envs.port}`);
 }
 bootstrap();
